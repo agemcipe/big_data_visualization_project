c6d10c5ad6dac16535b7de35a72442af225a65a3
agemcipe <jonathan.haas@arcor.de>
2021-01-19 13:42:45 +0100

    Add filtering by cluster size

--- main.R
+++ main.R
@@ -21,7 +21,7 @@ save_widget <- function(network, file_name) {
 }
 
 full_network_df <- read_csv("intermediate/preprocessed_data.csv")
-full_author_df <- read_csv("intermediate/authors.csv")
+full_author_df <- read_csv("intermediate/authors_joined.csv")
 full_author_df$idx <- seq.int(nrow(full_author_df)) - 1
 
 # sampled_network_df <- network_df[sample(nrow(network_df), 2000), ]
@@ -31,12 +31,15 @@ full_author_df$idx <- seq.int(nrow(full_author_df)) - 1
 # preselected_author <- full_author_df[sample(nrow(full_author_df), 500), ]
 preselected_author <- full_author_df[full_author_df$categories_first == "astro-ph",]
 
-preselected_author <- preselected_author[sample(nrow(preselected_author), nrow(preselected_author) / 4), ]
+preselected_author <- preselected_author[sample(nrow(preselected_author), 400), ]
 
 selected_network_df <- full_network_df[
     (full_network_df$author_id_x %in% preselected_author$author_id) |
     (full_network_df$author_id_x %in% preselected_author$author_id), 
 ]
+
+
+
 
 author_id_x_cnt = selected_network_df %>%
     group_by(author_id_x) %>%
@@ -62,12 +65,30 @@ selected_author_df <- full_author_df[
 selected_author_df <- selected_author_df %>%
     inner_join(author_pub_cnt)
 
-selected_author_df$idx <- seq.int(nrow(selected_author_df)) - 1
+selected_author_df$idx <- seq.int(nrow(selected_author_df)) # - 1
 
 joined <- selected_network_df %>%
     inner_join(selected_author_df[, c("author_id", "idx")], by = c("author_id_x" = "author_id")) %>%
     inner_join(selected_author_df[, c("author_id", "idx")], by = c("author_id_y" = "author_id"))
 
+############# filter by cluster size
+
+# igraph nodes have to be 1 indexed!
+g <- igraph::graph_from_edgelist(as.matrix(joined[, c("idx.x", "idx.y")]), directed = FALSE) 
+
+com <- components(g)
+# hist(component_distribution(g, cumulative = FALSE, mul.size = TRUE)) 
+
+selected_author_df$membership <- com$membership
+
+# FILTERING
+filtered_selected_author_df <- selected_author_df[selected_author_df$membership %in% which(com$csize > 10),]
+
+filtered_selected_author_df$idx <- seq.int(nrow(filtered_selected_author_df)) - 1
+
+filtered_joined <- selected_network_df %>%
+    inner_join(filtered_selected_author_df[, c("author_id", "idx")], by = c("author_id_x" = "author_id")) %>%
+    inner_join(filtered_selected_author_df[, c("author_id", "idx")], by = c("author_id_y" = "author_id"))
 
 # simple_network <- simpleNetwork(
 #     network_to_display,
@@ -77,13 +98,13 @@ joined <- selected_network_df %>%
 # simple_network
 
 force_network <- forceNetwork(
-    Links = joined,
-    Nodes = selected_author_df,
+    Links = filtered_joined,
+    Nodes = filtered_selected_author_df,
     Source = "idx.x",
     Target = "idx.y",
     Value = "cnt_publications",
     NodeID = "author_name",
-    Group = "categories_first",
+    Group = "affiliation",
     Nodesize = "auth_cnt_publications",
     radiusCalculation = JS("Math.sqrt(d.nodesize)+6"),
     linkDistance = JS("function(d){return 100/(d.value)}"),
@@ -91,12 +112,12 @@ force_network <- forceNetwork(
     zoom = TRUE
 )
 
-force_network
 
 file_name <- get_variable_name(force_network)
 save_widget(force_network, file_name)
 
 
+force_network
 #################### TEST ##################
 data(MisLinksagemcipe/data_processes_mlcolonoscopy)
 data(MisNodes)
@@ -107,3 +128,5 @@ forceNetwork(
     Value = "value", NodeID = "name",
     Group = "group", zoom = TRUE
 )
+
+################## Clusters #################
