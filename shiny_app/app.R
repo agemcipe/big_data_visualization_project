9c89750d6db0f4754c109f3f68685a3db4435350
agemcipe <jonathan.haas@arcor.de>
2021-01-22 18:16:28 +0100

    Add wip terms count per author

--- shiny_app/app.R
+++ shiny_app/app.R
@@ -5,46 +5,55 @@ library(networkD3)
 source("helpers.R")
 
 
+on_node_click <- 'Shiny.setInputValue("clicked_node_name", d.name, {priority: "event"});'
+
+
+
 #### Define server logic required to draw output
 server <- function(input, output) {
-  
+
   # Filter by author and select neighbors
   selected_author_df <- reactive({
-    # filter_by_author_and_neighbors(full_author_df, authors()[[1]],input$neighbors, full_graph) # "Marwala T.,Haas Martin"
-    if(length(input$authors) == 0) {return(full_author_df)}
-    filter_by_author_and_neighbors(full_author_df, input$authors,input$neighbors, full_graph) # "Marwala T.,Haas Martin"
+    if (length(input$authors) == 0) {
+      return(full_author_df)
+    }
+    filter_by_author_and_neighbors(full_author_df, input$authors, input$neighbors, full_graph) # "Marwala T.,Haas Martin"
   })
-  
+
   subgraph_author_df <- reactive({
-    if(input$full_subgraph){
+    if (input$full_subgraph) {
       authors_in_same_comp <- full_author_df[
         full_author_df$membership %in% selected_author_df()$membership,
-      ]  
+      ]
     }
     else {
       selected_author_df()
     }
   })
 
-  
+
   filtered_by_subject_author_df <- reactive({
-    if(length(input$subject_filter) == 0) {return(subgraph_author_df())}
+    if (length(input$subject_filter) == 0) {
+      return(subgraph_author_df())
+    }
     filtered_author_df <- subgraph_author_df() %>% filter(
       categories_first %in% input$subject_filter
     )
   })
-  
+
   filtered_author_df <- reactive({
-    filter_out_small_connected_components(full_network_df, 
-                                          filtered_by_subject_author_df(),
-                                          input$min_comp)
+    filter_out_small_connected_components(
+      full_network_df,
+      filtered_by_subject_author_df(),
+      input$min_comp
+    )
   })
-  
+
   links <- reactive({
     links_and_nodes <- prepare_final_links(full_network_df, filtered_author_df())
     links_and_nodes$links
   })
-  
+
   output$net <- renderForceNetwork(forceNetwork(
     Links = links(),
     Nodes = filtered_author_df(),
@@ -57,89 +66,124 @@ server <- function(input, output) {
     # radiusCalculation = JS("Math.sqrt(d.nodesize)+6"),
     # linkDistance = JS("function(d){return 100/(d.value)}"),
     # linkWidth = JS("function(d) { return Math.pow(d.value, 2); }"),
+    clickAction = on_node_click,
     zoom = TRUE,
     legend = TRUE,
     # colourScale = JS(ColourScale)
-  )
-  )
-  
+  ))
+
   number_of_nodes <- reactive({
     length(filtered_author_df())
   })
-  
+
+  output$selected_author_chart <- renderPlot(
+    make_author_terms_count_plot(input$authors)
+  )
+  output$clicked_node_chart <- renderPlot(
+    make_author_terms_count_plot(input$clicked_node_name)
+  )
+
   output$text <- renderText({
-    var <- paste("Your current options are: \n",
-                 "Authors: ", input$authors, "\n",
-                 "Number of neighbors: ", input$neighbors, "\n",
-                 "Subject filter: ", input$subject_filter, "\n",
-                 "Group by: ", input$group)
-    
-    if(number_of_nodes() > 10000) {paste("Graph too large. Please choose less than 10,000 nodes. \n", var)}
-    else if (number_of_nodes() == 0) {paste("No nodes selected.\n", var)}
-    else {paste("You have selected ", number_of_nodes(), " nodes.\n", var)}
+    var <- paste(
+      "Your current options are: \n",
+      "Authors: ", input$authors, "\n",
+      "Number of neighbors: ", input$neighbors, "\n",
+      "Subject filter: ", input$subject_filter, "\n",
+      "Group by: ", input$group
+    )
+
+    if (number_of_nodes() > 100) {
+      paste("Graph too large. Please choose less than 10,000 nodes. \n", var)
+    }
+    else if (number_of_nodes() == 0) {
+      paste("No nodes selected.\n", var)
+    }
+    else {
+      paste("You have selected ", number_of_nodes(), " nodes.\n", var)
+    }
   })
-  
 }
 
 # Define UI for app that draws a histogram ----
 ui <- fluidPage(
-  
+
   # App title ----
   titlePanel(h1("Co-author graph from the Arxiv Dataset ")),
-  
+
   # Sidebar layout with input and output definitions ----
   sidebarLayout(
-    
-    
+
+
     # Sidebar panel for inputs ----
     sidebarPanel(
       h3("Apply filters"),
-      selectizeInput("authors", label = "Select Authors", 
-                     choices = full_author_df$author_name,
-                     multiple = TRUE, 
-                     options = list(placeholder = 'Ashimov S. M.',
-                                    onInitialize = I('function() {this.setValue("Ashimov S. M.");}'))),
-      
+      selectizeInput("authors",
+        label = "Select Authors",
+        choices = full_author_df$author_name,
+        multiple = TRUE,
+        options = list(
+          placeholder = "Ashimov S. M.",
+          onInitialize = I('function() {this.setValue("Ashimov S. M.");}')
+        )
+      ),
+
       # Input: Slider for the number of neighbors ----
-      sliderInput(inputId = "neighbors",
-                  label = "Include neighbors of max degree:",
-                  min = 1,
-                  max = 10,
-                  value = 2),
-      
+      sliderInput(
+        inputId = "neighbors",
+        label = "Include neighbors of max degree:",
+        min = 1,
+        max = 10,
+        value = 2
+      ),
+
       # Input: Checkbox whether to include full subgraph
-      checkboxInput(inputId = "full_subgraph",
-                    label = "Include full subgraphs containing selected authors",
-                    value = FALSE),
-      
+      checkboxInput(
+        inputId = "full_subgraph",
+        label = "Include full subgraphs containing selected authors",
+        value = FALSE
+      ),
+
       # Input: Filter by subject area
-      selectizeInput(inputId = "subject_filter", 
-                     label = "Filter by subject area", 
-                     choices = full_author_df$categories_first,
-                     multiple = TRUE),
-      
+      selectizeInput(
+        inputId = "subject_filter",
+        label = "Filter by subject area",
+        choices = full_author_df$categories_first,
+        multiple = TRUE
+      ),
+
       # Input: Filter  small connected components  ----
-      sliderInput(inputId = "min_comp",
-                  label = "Minimal size of connected graph components:",
-                  min = 1,
-                  max = 10,
-                  value = 1),
-      
+      sliderInput(
+        inputId = "min_comp",
+        label = "Minimal size of connected graph components:",
+        min = 1,
+        max = 10,
+        value = 1
+      ),
+
       h3("Change aesthetics"),
-      
+
       # Input: Group by
-      selectInput("group", label = "Select color idiom",
-                  choices = list("Subject area" = "categories_first","E-mail domain" = "email_domain", 
-                                "Affilitation" = "affiliation"),
-                  selected = "categories_first"),
+      selectInput("group",
+        label = "Select color idiom",
+        choices = list(
+          "Subject area" = "categories_first", "E-mail domain" = "email_domain",
+          "Affilitation" = "affiliation"
+        ),
+        selected = "categories_first"
+      ),
       width = 2
-      
     ),
-    
+
     # Main panel for displaying outputs ----
     mainPanel(
-      forceNetworkOutput(outputId = "net"),
-      verbatimTextOutput('text')
+      verticalLayout(
+        forceNetworkOutput(outputId = "net"),
+        verbatimTextOutput("text"),
+        splitLayout(
+          plotOutput("selected_author_chart"),
+          plotOutput("clicked_node_chart")
+        )
+      )
     )
   )
 )
